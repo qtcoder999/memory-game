8f5c64851b50dcb7d76ae61eee5be732e691ccc9
Paras Anand <paras.anand.uni@cars24.com>
2020-07-06 01:20:09 +0530

    Redux persist integrated

--- App/Redux/index.js
+++ App/Redux/index.js
@@ -1,6 +1,8 @@
 import {combineReducers} from 'redux';
+import {persistReducer} from 'redux-persist';
 import configureStore from './CreateStore';
 import rootSaga from '../Sagas/';
+import ReduxPersist from '../Config/ReduxPersist';
 
 /* ------------- Assemble The Reducers ------------- */
 export const reducers = combineReducers({
@@ -9,8 +11,15 @@ export const reducers = combineReducers({
 });
 
 export default () => {
+  let finalReducers = reducers;
+  // If rehydration is on use persistReducer otherwise default combineReducers
+  if (ReduxPersist.active) {
+    const persistConfig = ReduxPersist.storeConfig;
+    finalReducers = persistReducer(persistConfig, reducers);
+  }
+
   let {store, sagasManager, sagaMiddleware} = configureStore(
-    reducers,
+    finalReducers,
     rootSaga,
   );
 
